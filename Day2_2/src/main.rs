ec2f9e6eb8d441afae0f153e19c1745d37b907ef
random-sir <antoni.torres@outlook.com>
2024-12-04 00:51:24 -0300

    Finished 2-2

--- Day2_2/src/main.rs
+++ Day2_2/src/main.rs
@@ -1,6 +1,25 @@
-use std::fs;
 use itertools::Itertools;
+use std::fs;
 
+fn is_safe(vec: &Vec<i32>) -> bool {
+    if vec[0] > vec[1] {
+        for (a, b) in vec.iter().tuple_windows() {
+            let diff = a - b;
+            if diff < 1 || diff > 3 {
+                return false;
+            }
+        }
+    } else {
+        for (a, b) in vec.iter().tuple_windows() {
+            let diff = b - a;
+            if diff < 1 || diff > 3 {
+                return false;
+            }
+        }
+    }
+
+    true
+}
 fn main() {
     let file = fs::read_to_string("input.txt").unwrap();
     println!("{}", file);
@@ -8,27 +27,28 @@ fn main() {
     let lines = file.lines();
 
     let mut safe_count = 0;
-    'outer: for line in lines {
-        let line :Vec<i32> = line.split_whitespace().map(|num| num.parse().unwrap()).collect();
+    for line in lines {
+        let line: Vec<i32> = line
+            .split_whitespace()
+            .map(|num| num.parse().unwrap())
+            .collect();
 
-        if line[0] > line[1] {
-            for (a, b) in line.iter().tuple_windows(){
-                let diff = a - b;
-                if diff < 1 || diff > 3 {
-                    continue 'outer
+        let mut flag = false;
+        if is_safe(&line) {
+            flag = true;
+        } else {
+            let size = line.len();
+            line.into_iter().combinations(size - 1).for_each(|vec| {
+                if is_safe(&vec) {
+                    flag = true;
                 }
-            }
-        }
-        else {
-            for (a, b) in line.iter().tuple_windows(){
-                let diff = b - a;
-                if diff < 1 || diff > 3 {
-                    continue 'outer
-                }
-            }
+            });
         }
 
-        safe_count+= 1;
+        if flag {
+            safe_count += 1;
+        }
     }
+
     println!("{}", safe_count);
 }
